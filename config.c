664f1a5d1da30b763fbfb235c162e2d1371c0d49
Isaac Rose <isaacdrose@gmail.com>
2017-12-14 15:38:41 -0500

    Added basic project files

---
+++ config.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   config.c
+ * Author: Isaac.Rose
+ *
+ * Created on December 14, 2017, 2:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ main.c
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.c
+ * Author: Isaac.Rose
+ *
+ * Created on December 14, 2017, 2:30 PM
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "config.h"
+/*
+ * 
+ */
+
+
+int main(int argc, char** argv) {
+    uint8_t myByte;
+    uint16_t myWord;
+    
+    while(1){
+        myByte++;
+        myWord--;
+    }
+    return (EXIT_SUCCESS);
+}
+
